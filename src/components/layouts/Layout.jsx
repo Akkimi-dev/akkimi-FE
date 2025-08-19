dedd916c6ba5992eee6fb0473862470cfcf7d15b
chan000518 <wkdvnddl050701@gmail.com>
2025-08-20 01:21:13 +0900

    fix: 레이아웃 수정 - 모달 용 포달 추가 - 반응형 420로 기준 변경

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -2,11 +2,12 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="h-[100dvh] flex justify-center items-center bg-black">
+    <div className="relative h-[100dvh] flex justify-center items-center bg-black">
       {/* 안쪽: 실제 앱 캔버스. 모바일 퍼스트: 가로는 100%, 데스크탑에서는 최대 420px로 고정 */}
       <div
-        className="w-full max-w-[420px] bg-white h-[100dvh] sm:h-[700px]"
+        className="w-full max-w-[420px] bg-white h-[100dvh] xs:h-[700px]"
       >
+        <div id="layout-portal" className="absolute inset-0" />
         <Outlet />
       </div>
     </div>
