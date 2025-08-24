b69118951d7a370c11f664709c094c20292fbbb4
chan000518 <wkdvnddl050701@gmail.com>
2025-08-24 23:32:51 +0900

    fix: 헤더 디자인 깨지는 거 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({header}) {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed sm:absolute sm:inset-x-0 top-0 z-50 w-full h-12 flex justify-between items-center bg-bg-blue">
+    <div className="absolute inset-x-0 top-0 z-50 w-full h-12 flex justify-between items-center bg-bg-blue">
       <button className="px-4" onClick={() => navigate(-1)}>
         <BackArrow />
       </button>
